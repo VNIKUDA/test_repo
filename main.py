ec223abf1b12c367ad46084aeadb1f4727011e8f
Олександр Олександрович <aleksvnikuda2008@gmail.com>
2024-04-20 13:54:41 +0300

    SQL database added to API

--- main.py
+++ main.py
@@ -1,34 +1,103 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import sqlite3
+import json
 
-
-app = FastAPI()
-
+# Форма користувача
 class User(BaseModel):
-    id: int 
     username: str
     email: str
 
-users = list()
-user_count = 0
+# Частина для роботи з базою данних
+database = "users.db"
+conn = None
+cursor = None
 
+# Відкриття та закриття бази даних
+def open():
+    global conn, cursor
+    conn = sqlite3.connect(database)
+    cursor = conn.cursor()
+
+def close():
+    cursor.close()
+    conn.close()
+
+# SQL запит
+def do(query):
+    cursor.execute(query)
+    conn.commit()
+
+# Створення всіх таблиць (в даному випадку тільки users)
+def create_all():
+    open()
+
+    do('''CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY, 
+            username VARCHAR, 
+            email VARCHAR
+       )''')
+
+    close()
+
+# Додавання користувача
+def add_user(user: User):
+    open()
+
+    cursor.execute("INSERT INTO users (username, email) VALUES (?, ?)", [user.username, user.email])
+    conn.commit()
+
+    close()
+
+    return user
+
+# Пошук користувача по id
+def get_user(user_id: int):
+    open()
+
+    cursor.execute("SELECT * FROM users WHERE id == ?", (str(user_id), ))
+    user = cursor.fetchall()[0]
+
+    close()
+
+    return user
+
+# Всі користувачі
+def users():
+    open()
+
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+
+    close()
+
+    return users
+
+# Ініціалізація бази даних
+create_all()
+
+
+# FastAPI 
+app = FastAPI()
+
+# Створення користувача
 @app.post("/create_user")
 def create_user(username: str, email: str):
-    global user_count
-    new_user = User(id=user_count, username=username, email=email)
-    users.append(new_user)
-    user_count += 1 
+    new_user = User(username=username, email=email)
 
-    return new_user
+    return add_user(new_user)
 
+# Користувач по id 
 @app.get("/users/{user_id}")
 def get_user(user_id: int):
-    for user in users:
-        if user.id == user_id:
+    users_ = users()
+    for user in users_:
+        if user[0] == user_id:
             return user
         
     raise HTTPException(status_code=400, detail="Такого користувача нема")
 
+# Всі користувачі
 @app.get("/users")
 def get_all_users():
-    return users
+    return users()
